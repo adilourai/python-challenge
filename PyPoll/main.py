a972e1583fb2d46b1b96987bc88fcf6b9318797b
adilourai <adinaraizen@gmail.com>
2023-03-08 20:43:33 -0700

    Final homework files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,24 +13,63 @@ with open("C:/Users/adina/Desktop/Classwork/Module 3 Python/Module 3 Challenge/p
 
 #Calculate total number of votes
 total_votes = float(len(votes))
+    
+#Create list of candidates who received votes; alphabetize list to keep results consistent
+candidates = list(set(votes))
+candidates.sort()
 
-#Create list of candidates who received votes
-candidates_set = set(votes)
-candidates = list(candidates_set)
-
-#Calculate total votes for each candidate
+#Calculate total votes for each candidate using a vote counting function and storing values in a dictionary
 votes_total_dict = {}
-for name in candidates:
-    candidate_total = votes.count(name)
-####THIS IS WHERE IT BREAKS!!! GOTTA FIX THE DICTIONARY###
-for i in range(len(candidates)):
-    votes_total_dict[candidates[i]] = str(candidate_total)[i]
 
-#Calculate percentage of votes won by each candidate
+def candidate_votes(name):
+    vote_count = 0
+    for vote in votes:
+        if vote == name:
+            vote_count += 1
+    votes_total_dict[name] = vote_count
+
+for name in candidates:
+    candidate_votes(name)
+
+#Calculate percentage of votes won by each candidate using a function to calculate and then format as percent, and store in dictionary
 votes_percent_dict = {}
-for name in candidates:
-    candidate_percent = round((float(votes.count(name) / total_votes)),2)
-    for i in range(len(candidates)):
-        votes_percent_dict[candidates[i]] = str(candidate_percent)[i]
 
-# print(votes_total_dict["Diana DeGette"])
+def candidate_percentages(name):
+    percent = str(round(((votes_total_dict[name] / total_votes) * 100),3)) + "%"
+    votes_percent_dict[name] = percent
+
+for name in candidates:
+    candidate_percentages(name)
+
+#Determine winner
+most_votes = max(votes_total_dict.values())
+winner = {name for name in votes_total_dict if votes_total_dict[name]==most_votes}
+
+#Print results to terminal
+print("Election Results\n")
+print("--------------------\n")
+print("Total Votes: " + str(int(total_votes)) + "\n")
+print("--------------------\n")
+for name in candidates:
+    print(f'{name}: {votes_percent_dict[name]} ({votes_total_dict[name]})\n')
+print("--------------------\n")
+print("Winner: ",end="")
+print(*winner)
+print("\n--------------------")
+
+#Create output .txt file and write results into file
+output_file = open("C:/Users/adina/Desktop/Classwork/Module 3 Python/Module 3 Challenge/python-challenge/PyPoll/Analysis/PyPoll_Analysis.txt", "w")
+
+output_file.write
+output_file.write("Election Results\n")
+output_file.write("--------------------\n")
+output_file.write("Total Votes: " + str(int(total_votes)) + "\n")
+output_file.write("--------------------\n")
+for name in candidates:
+    output_file.write(f'{name}: {votes_percent_dict[name]} ({votes_total_dict[name]})\n')
+output_file.write("--------------------\n")
+output_file.write("Winner: ")
+output_file.write(*winner)
+output_file.write("\n--------------------")
+
+output_file.close()
